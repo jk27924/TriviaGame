867bf9fbd0e28d2dae2fffdc1f01fce631b7f9ef
Jihong Kim <jk279242naver.com>
2019-06-26 06:08:38 -0500

    I added Pseudo coding on js file.

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+// <<PSEUDO CODING FIRST>>
+
+// SHOW vs. HIDE
+    // * Start Button vs. Timer
+    // * Questions vs. Results
+    // * Submit Button vs. Restart Button
+
+//------------------------------------------------------------------//
+
+// 1. Click Start Button to start the Game
+    // Hide Start Button / Show Timer
+
+// 2. Set the Timer at 60 seconds, and count down by 1 second
+    // It decrements, and stops at 0
+
+// 3 - a. 
+// When timer is Stopped,
+    // Evaluate the Answers
+    // Hide Timer
+    // Hide Questions / Show Results
+    // Hide Submit Button / Show Restart Button
+
+// 3 - b. 
+// When quiz is done before timer stops,
+    // Click Submit Button
+        // Evaluate the Answers
+        // Hide Timer
+        // Hide Questions / Show Results
+        // Hide Submit Button / Show Restart Button
+
+// 4. Click Restart button,
+    // Hide Timer / Show Start Button
+    // Hide Results / Show Questions
+    // Hide Restart Button / Show Submit Button
+
+
+$(document).ready(function () {
+
+
+
+
+
+    
+});
